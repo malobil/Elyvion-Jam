5d6777fdca572f41b73c670157e37604facceb17
Kevin_Ibanez <kevin.ibanezpro@gmail.com>
2020-04-11 00:22:07 +0200

    Pushable objs

--- Assets/Scripts/Interact/TestObjectInteractible.cs
+++ Assets/Scripts/Interact/TestObjectInteractible.cs
@@ -4,27 +4,73 @@ using UnityEngine;
 
 public class TestObjectInteractible : MonoBehaviour, IInteractable
 {
+    [SerializeField] private float m_MovingRange = 2f;
     enum MyDirection {Front,Back,Left,Right}
+    Rigidbody RBComp;
+
+    bool IsPush = false;
+    Vector3 PushDirection;
+    Vector3 BasePushPosition;
     
     public void Interact()
     {
-        MyDirection PlayerDir = MyDirection.Front ;
+        if(!IsPush)
+        {
+            switch (GetPushDirection())
+            {
+                case MyDirection.Front:
+                    PushDirection = -transform.forward;
+                    break;
+                case MyDirection.Back:
+                    PushDirection = transform.forward;
+                    break;
+                case MyDirection.Right:
+                    PushDirection = -transform.right;
+                    break;
+                case MyDirection.Left:
+                    PushDirection = transform.right;
+                    break;
+            }
+        }
+
+        if(CheckIfCanPush())
+        {
+            BasePushPosition = transform.position;
+            IsPush = true;
+        }
+    }
+
+    bool CheckIfCanPush()
+    {
+        RaycastHit hit;
+
+        if (!Physics.Raycast(transform.position, PushDirection, out hit, m_MovingRange))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    MyDirection GetPushDirection()
+    {
+        MyDirection PlayerDir = MyDirection.Front;
         GameObject Player = GameObject.FindGameObjectWithTag("Player");
         Vector3 Direction = Player.transform.position - transform.position;
-        float DotForward = Vector3.Dot(transform.forward,Direction);
-        float DotRight = Vector3.Dot(transform.right,Direction);
 
-        if(Mathf.Abs(DotForward) >= Mathf.Abs(DotRight))
+        float DotForward = Vector3.Dot(transform.forward, Direction);
+        float DotRight = Vector3.Dot(transform.right, Direction);
+
+        if (Mathf.Abs(DotForward) >= Mathf.Abs(DotRight))
         {
             if (DotForward > 0)
             {
                 PlayerDir = MyDirection.Front;
-                Debug.Log("In front");
             }
             else if (DotForward < 0)
             {
                 PlayerDir = MyDirection.Back;
-                Debug.Log("In Back");
             }
         }
         else
@@ -32,41 +78,38 @@ public class TestObjectInteractible : MonoBehaviour, IInteractable
             if (DotRight > 0)
             {
                 PlayerDir = MyDirection.Right;
-                Debug.Log("In right");
             }
             else if (DotRight < 0)
             {
                 PlayerDir = MyDirection.Left;
-                Debug.Log("In left");
             }
-        }  
-        
-        switch(PlayerDir)
+        }
+
+        return PlayerDir;
+    }
+
+    void Push(Vector3 PushDirection)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, (BasePushPosition + (PushDirection* m_MovingRange)),1f *Time.deltaTime);
+
+        if(transform.position == BasePushPosition + (PushDirection * m_MovingRange))
         {
-            case MyDirection.Front:
-                transform.position = Vector3.MoveTowards(transform.position, transform.position -transform.forward,1f);
-                break;
-            case MyDirection.Back:
-                transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.forward, 1f);
-                break;
-            case MyDirection.Right:
-                transform.position = Vector3.MoveTowards(transform.position, transform.position - transform.right, 1f);
-                break;
-            case MyDirection.Left:
-                transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.right, 1f);
-                break;
+            IsPush = false;
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        RBComp = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(IsPush)
+        {
+            Push(PushDirection);
+        }
     }
 }

6b694b374970e66bb556d5796904bfff74b487e6
Kevin_Ibanez <kevin.ibanezpro@gmail.com>
2020-04-11 15:55:04 +0200

    Detect file creation

---
+++ Assets/Scripts/Environment/Torch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : FileChecker
+{
+    [SerializeField] private GameObject FireEffect;
+
+    public override void FileCreatedDetected()
+    {
+        base.FileCreatedDetected();
+        FireEffect.SetActive(true);
+    }
+
+    public override void FileRemoved()
+    {
+        base.FileRemoved();
+        FireEffect.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/ErrorScripts/FileChecker.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public abstract class FileChecker : MonoBehaviour
+{
+    public string FileName;
+    public string FileContent;
+
+    private string ErrorFolderPath;
+    private bool FileExist = false;
+
+    public virtual void OnEnter()
+    {
+        ErrorFolderPath = Directory.GetParent(Application.dataPath) + "/ERRORS/";
+    }
+
+    public virtual void OnUpdate()
+    {
+
+    }
+
+    public virtual void FileCreatedDetected()
+    {
+        Debug.Log("change");
+        FileExist = true;
+    }
+
+    public virtual void FileRemoved()
+    {
+        Debug.Log("change");
+        FileExist = false;
+    }
+
+    void CheckFileExist()
+    {
+        if(FileExist)
+        {
+            if (!File.Exists(ErrorFolderPath + FileName))
+            {
+                FileRemoved();
+            }
+        }
+        else
+        {
+            if (File.Exists(ErrorFolderPath + FileName))
+            {
+                FileCreatedDetected();
+            }
+        }
+      
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnEnter();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckFileExist();
+    }
+}
